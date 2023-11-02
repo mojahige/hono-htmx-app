35a591414ea1f1077f9fa0f61535b4d73f6e80e0
mojahige <13248811+mojahige@users.noreply.github.com>
2023-11-03 01:21:32 +0900

    Remove unused API root

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@
 /** @jsxFrag Fragment */
 import { Hono } from "hono/mod.ts";
 import { jsx, prettyJSON, serveStatic } from "hono/middleware.ts";
-import { api } from "@/api/index.ts";
 import { HomePage } from "@/components/pages/home/index.tsx";
 import { posts } from "@/routes/posts/index.tsx";
 
@@ -10,9 +9,6 @@ const app = new Hono();
 
 app.use("*", prettyJSON());
 app.use("/static/*", serveStatic({ root: "./" }));
-
-// TODO: Remove unused route
-app.route("/api", api);
 
 app.get("/", (c) => c.html(<HomePage />));
 app.route("/posts", posts);
